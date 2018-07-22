d1d6422e0d748264fdd6f49899e8a2908bf37a2f
Vladislav <vlad.unchuris@gmail.com>
2018-07-23 00:22:58 +0300

    added drawer

--- app/src/main/java/unchuris/vladislav/expensetracker/MainActivity.kt
+++ app/src/main/java/unchuris/vladislav/expensetracker/MainActivity.kt
@@ -1,20 +1,34 @@
 package unchuris.vladislav.expensetracker
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.res.Configuration
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.widget.Toast
 import unchuris.vladislav.expensetracker.ui.transaction.PostListViewModel
 import unchuris.vladislav.expensetracker.databinding.ActivityMainBinding
 import unchuris.vladislav.expensetracker.ui.money.MoneyListModel
+import com.ncapdevi.fragnav.FragNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+        , NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var viewModel: PostListViewModel
-
     private lateinit var moneyModel: MoneyListModel
 
 
@@ -32,5 +46,52 @@ class MainActivity : AppCompatActivity() {
         moneyModel = ViewModelProviders.of(this).get(MoneyListModel::class.java)
         binding.moneyModel = moneyModel
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+
+        toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_wallet -> Toast.makeText(this, "wallet", Toast.LENGTH_SHORT).show()
+            R.id.nav_settings -> Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show()
+            R.id.nav_log_out -> Toast.makeText(this, "log_out", Toast.LENGTH_SHORT).show()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
